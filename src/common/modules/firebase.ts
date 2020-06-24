7b50ecefe216fb10aaa94be27ba78c633ab8c5d0
indegser <indegser@gmail.com>
2020-06-24 23:47:43 +0900

    Add firebase

---
+++ src/common/modules/firebase.ts
@@ -0,0 +1,9 @@
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+
+firebase.apps.length === 0 &&
+  firebase.initializeApp(JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG));
+
+export const db = firebase.firestore();
