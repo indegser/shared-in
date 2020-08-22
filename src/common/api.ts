ed46052ad58eb8dbc5d110e3cbf059095bf66014
indegser <indegser@gmail.com>
2020-08-22 19:29:18 +0900

    Restore ConnectGithub to use GithubProvider

--- src/common/api.ts
+++ src/common/api.ts
@@ -1,4 +1,6 @@
-import { db } from "./modules/firebase";
+import firebase from "firebase/app";
+const db = firebase.firestore();
+import { authStoreApi } from "./store";
 
 const converQuerySnapshotToArrayOfDocs = <T>(
   querySnapshot: firebase.firestore.QuerySnapshot
@@ -10,12 +12,56 @@ const converQuerySnapshotToArrayOfDocs = <T>(
 };
 
 const fetchShares = async () =>
-  db
+  firebase
+    .firestore()
     .collection("shares")
     .orderBy("createdAt", "desc")
     .get()
     .then((snapshot) => converQuerySnapshotToArrayOfDocs<IShare>(snapshot));
 
+const fetchOpenGraph = async (url: string) => {
+  const response = await fetch("/api/og", {
+    method: "POST",
+    body: JSON.stringify({ url }),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+
+  const json = await response.json();
+  return json;
+};
+
+const shareApis = {
+  createShare: (share: IShare) => {
+    return db.collection("shares").add(share);
+  },
+};
+
+const userApis = {
+  connectGithub: () => {
+    const provider = new firebase.auth.GithubAuthProvider();
+    return firebase.auth().signInWithPopup(provider);
+  },
+  createUser: (user: IUser) => {
+    return db.collection("users").doc(user.uid).set(user);
+  },
+  updateUserBio: ({ team, company }) => {
+    const { user } = authStoreApi.getState();
+    if (user.team === team && user.company === company) {
+      return;
+    }
+
+    return db
+      .collection("users")
+      .doc(user.uid)
+      .set({ team, company }, { merge: true });
+  },
+};
+
 export default {
+  fetchOpenGraph,
   fetchShares,
+  ...shareApis,
+  ...userApis,
 };
